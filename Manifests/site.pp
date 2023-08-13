1d870be3c50ab3863f0af9cd79030a0be1a90d8a
MaxChernoshey <89794858+MaxChernoshey@users.noreply.github.com>
2023-08-13 15:04:49 +0300

    Update site.pp

--- Manifests/site.pp
+++ Manifests/site.pp
@@ -1,1 +1,48 @@
+node slave1.puppet {
+  package { 'httpd':
+    ensure => installed, 
+  }
+  
+  service { 'httpd': # описываем сервис 'httpd'
+    ensure => running, # он должен быть запущен
+    enable => true, # его нужно запускать автоматически при старте системы
+  }
+  
+  service { 'firewalld':
+    ensure => stopped,
+    enable => false,
+  }
+  
+  file { '/var/www/html/index.html': 
+    path => '/var/www/html/index.html',
+    source => 'https://raw.githubusercontent.com/MaxChernoshey/itacademy-devops-files/master/02-tools/index.html',
+  }
+}
 
+node slave2.puppet {
+  package { 'php':
+    ensure => installed,
+  }
+  package { 'httpd':
+    ensure => installed,
+  }
+  service { 'httpd': # описываем сервис 'httpd'
+    ensure => running, # он должен быть запущен
+    enable => true, # его нужно запускать автоматически при старте системы
+  }
+  service { 'firewalld':
+    ensure => stopped,
+    enable => false,
+  }
+  file { '/var/www/html/index.html':
+    ensure  => absent,
+  }
+  
+  file { '/var/www/html/index.php': 
+   path => '/var/www/html/index.php',
+   source => 'https://raw.githubusercontent.com/MaxChernoshey/itacademy-devops-files/master/02-tools/index.php',
+  }
+}
+node mineserver.puppet {
+  mod 'puppet-nginx', '5.0.0'
+  }
