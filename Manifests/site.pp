3f1980a635f614e7d023e453aec14c504c6969ff
MaxChernoshey <89794858+MaxChernoshey@users.noreply.github.com>
2023-08-13 20:35:53 +0300

    Update site.pp

--- Manifests/site.pp
+++ Manifests/site.pp
@@ -1,6 +1,15 @@
 node master.puppet {
   class 'nginx',
   }
+
+  nginx::resource::server { '192.168.50.5':
+  listen_port => 81,
+  proxy       => 'http://192.168.50.10/',
+  }
+  nginx::resource::server { '192.168.50.5':
+  listen_port => 82,
+  proxy       => 'http://192.168.50.15/',
+}
   
 node slave1.puppet {
   package { 'httpd':
