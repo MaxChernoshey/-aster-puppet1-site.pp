97d1a38a4ab73ffc6536ec9d724cf73cafc8dde5
MaxChernoshey <89794858+MaxChernoshey@users.noreply.github.com>
2023-09-26 21:39:36 +0300

    Update init.pp

--- modules/mainkraft/manifest/init.pp
+++ modules/mainkraft/manifest/init.pp
@@ -6,14 +6,14 @@ class mainkraft {
   }
   
   file { 'minekraft':
-    path=> '/opt/minekraft/minekraft',
+    path => '/opt/minekraft/minekraft',
     source => 'https://piston-data.mojang.com/v1/objects/84194a2f286ef7c14ed7ce0090dba59902951553/server.jar',
     mode => '755',
     require => File['/opt/minekraft'],
   }
     
   file { 'minekraft_service':
-    path=> '/etc/systemd/system/minekraft.service',
+    path => '/etc/systemd/system/minekraft.service',
     source => 'puppet:///modules/minekraft/minekraft.service',
     notify => Service ['minekraft']
   }
